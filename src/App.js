63fd7349ff124ccfad3694877a544a6acde6804d
PrasanthAndroiddeveloper <prasanna@natobotics.com>
2021-05-20 12:29:59 +0530

    First commit e commerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Homescreen from "./screens/Homescreen";
+import Productsdescscreen from "./screens/Productsdescscreen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <BrowserRouter>
+        <Route path="/" component={Homescreen} exact />
+        <Route path="/product/:id" component={Productsdescscreen} exact />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/screens/Productsdescscreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Productsdescscreen() {
+  return (
+    <div>
+      <h1>Products Desription </h1>
+    </div>
+  );
+}
