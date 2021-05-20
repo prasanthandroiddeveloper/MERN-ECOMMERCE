688841de3158d149e18cd3bd7cd61342b5bf6d85
PrasanthAndroiddeveloper <prasanna@natobotics.com>
2021-05-20 12:39:38 +0530

    Second Comitt

--- src/screens/Productsdescscreen.js
+++ src/screens/Productsdescscreen.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Productsdescscreen() {
   return (
     <div>
-      <h1>Products Desription </h1>
+      <h1>Products Desription Changed</h1>
     </div>
   );
 }
