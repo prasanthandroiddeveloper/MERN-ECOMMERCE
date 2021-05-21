b806e0aeb33e1811b808e5d8d110baf320f41fbf
PrasanthAndroiddeveloper <prasanna@natobotics.com>
2021-05-21 19:46:53 +0530

    friday commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "react-rating";
 
 export default function Product({ product }) {
   return (
-    <div className="col-md-3 m-5 card p-2">
+    <div className="col-md-3 m-5 card p-2 div">
       <div>
-        <img src={product.image} className="img-fluid" />
-        <h1>{product.name}</h1>
-        <h1>Rating: {product.rating}</h1>
-        <h1>Price: {product.price}</h1>
+        <Link to={`product/${product.id}`}>
+          <img src={product.image} className="img-fluid" />
+          <h1>{product.name}</h1>
+          <Rating
+            initialRating={product.Rating}
+            emptySymbol="fa fa-star-o fa-2x"
+            fullSymbol="fa fa-star fa-2x"
+          />
+          <h1>Price: {product.price}</h1>
+        </Link>
       </div>
     </div>
   );
